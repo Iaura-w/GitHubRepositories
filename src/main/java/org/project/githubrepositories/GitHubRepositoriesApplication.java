3c147d047852e78956c8d1c32256d34062ebf5e7
Iaura-w <laura.wo19@gmail.com>
2024-07-26 13:20:35 +0200

    Initial commit

---
+++ src/main/java/org/project/githubrepositories/GitHubRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package org.project.githubrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubRepositoriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/githubrepositories/GitHubRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.githubrepositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepositoriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
