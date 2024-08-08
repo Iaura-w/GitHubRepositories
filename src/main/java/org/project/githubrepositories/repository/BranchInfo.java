1969e54e086115f00b0c16c34abf4dd9ed8133ae
Iaura-w <laura.wo19@gmail.com>
2024-08-08 19:22:37 +0200

    refactor

--- src/main/java/org/project/githubrepositories/repository/BranchInfo.java
+++ src/main/java/org/project/githubrepositories/repository/BranchInfo.java
@@ -1,17 +1,7 @@
 package org.project.githubrepositories.repository;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-public class BranchInfo {
-    private String name;
-    private String commitSha;
+public record BranchInfo(
+        String name,
+        String commitSha
+) {
 }
